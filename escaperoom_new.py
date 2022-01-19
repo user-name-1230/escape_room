9ef19d3df642f2494957a7d79a9e59fef03bdcb7
ThomasSeider <97517117+ThomasSeider@users.noreply.github.com>
2022-01-19 15:01:38 +0100

    Raum 1 ausgebaut
    Poster, Sicherheitsausrüstung, Brecheisen eingebaut

--- escaperoom_new.py
+++ escaperoom_new.py
@@ -1,59 +1,110 @@
+# TODO
+# Fehlermeldungen bei unbekannten Befehlen
+# help Befehl deaktivieren? siehe ganz unten
+# Beschreibung Räume, über Einleitungsstory
+
 from PIL import Image
 import time
 from adventurelib import Room, when, say, start, Bag, Item, set_context
 import sys
 
+
 ### Globale Variablen ###
-room_number = 1
 sicherheitstuer_offen = False
 ransomware_passwort_eingegeben = False
+check_sicherheitsausruestung = False
+brecheisen_schienbein = False
 
-
+# unbenutzt...aus alter Version
 can_check_sim_slot = False
 sim_schrank_offen = False
 can_use_pin = False
 klappe_offen = False
 zugriff_computer = False
 status_gesehen = False
-check_sicherheitsausruestung = False
 
-### Befehle: umschauen anschauen nehmen benutzen öffnen 'verwende mit' Inventar help quit 'Raum verlassen'
+
+
+### allgemeine Befehle ### fuer jeden Raum
+
+def no_command_matches(command):
+    print("Das habe ich nicht verstanden")
+    # das funktioinert leider nicht. Hinweise auf Befehl: [Hilfe] hinzufügen
+    # siehe https://adventurelib.readthedocs.io/en/stable/customising.html
+
 
 @when ("hilfe")
 def zeige_befehle():
-    print("[hilfe] [umschauen] [anschauen] [nehmen] [benutzen] [öffnen] [Verwende mit]\n[Inventar] [Raum verlassen]\n\n[help] (sehr große Hilfe) [quit]\nauf Groß- und Kleinschreibung wird kein Wert gelegt ;-)")
+    print("Folge Befehle sind möglich:\n[hilfe] [umschauen] [anschauen] [nehmen] [benutzen] [öffnen] [Verwende mit]\n[Inventar] [Raum verlassen]\n\n[help] (sehr große Hilfe) [quit]\nauf Groß- und Kleinschreibung wird kein Wert gelegt ;-)")
+# Befehle: umschauen anschauen nehmen benutzen öffnen 'verwende mit' Inventar help quit 'Raum verlassen'
 
-@when("raum verlassen") ## aktuell noch keine Auswahl...
+@when ("nehmen")
+@when("nimm")
+def nehmen():
+    print("Was möchtest du nehmen?")
+
+@when ("benutzen")
+@when("benutze")
+def benutzen():
+    print("Was möchtest du benutzen?")
+
+@when ("anschauen")
+@when("schaue an")
+def anschauen():
+    print("Was möchtest du anschauen?")
+
+@when ("öffnen")
+@when("öffne")
+def oeffnen():
+    print("Was möchtest du öffnen?")
+
+@when ("verwende mit")
+@when("verwenden mit")
+def verwende_mit():
+    print("Was möchtest womit verwenden?")
+    print("zB verwende Smartphone mit dir")
+
+
+@when("raum verlassen") ## aktuell nur Maschinenraum
 def raum_verlassen():
+    global room_number
+    global sicherheitstuer_offen
     if (room_number == 1 and sicherheitstuer_offen == False):
         print("Die Tür ist verschlossen. Du kannst den Raum nicht verlassen")
     else:
         print("Du hast folgende Möglichkeiten:")
-        print("Kontrollraum (1)")
-#        print("Wohin? zB 1")
-        raum = int(input("Wohin? zB 1"))
-#        raum = int(raum)
-#        if (raum == room_number)
-#            print("Du befindest dich schon in diesem Raum!"
-        print("Du gehst in den Kontrollraum")
-
+        print("(1) Kontrollraum")
+        while 1:
+            wohin = input("Wohin? Bitte Nummer eingeben:")
+            if (wohin == "1"):
+                break
+            else:
+                print("dies ist kein Raum, bitte Nummer eingeben, zB 1")
+        wohin = int (wohin)
+        if (wohin == room_number):
+            print("Du befindest dich schon in diesem Raum!")
+        else:
+            print("Du gehst in den Raum")
+            room_number = wohin
+# umschauen einbauen, als Hinweis in welchem Raum jetzt
 
 ### Inventar ###
 # Liste möglicher Items
 brecheisen = Item("brecheisen")
 smartphone = Item("smartphone")
+# ab hier unbenutze Items
 haarnadel = Item("haarnadel")
 simkarte = Item("simkarte")
 inventory = Bag()
 
 # Inventar Grundlagen
 @when("inventar")
-#@when("inventar benutzen")  # benutzen
-#@when("benutze inventar")
-#@when("inventar anschauen")  # anschauen
-#@when("anschauen inventar")
-#@when("öffne inventar")  # öffnen
-#@when("inventar öffnen")
+@when("inventar benutzen")  # benutzen
+@when("benutze inventar")
+@when("inventar anschauen")  # anschauen
+@when("anschauen inventar")
+@when("öffne inventar")  # öffnen
+@when("inventar öffnen")
 def zeige_inventar():
     print("Du hast: ")
     if not inventory:
@@ -62,8 +113,53 @@ def zeige_inventar():
     for item in inventory:
         print(f"* {item}")
 
-### Räume ###
+### Smartphone Grundlagen
+@when("smartphone")
+@when("anschauen smartphone")
+@when("smartphone anschauen")
+def zeige_smartphone():
+    print("Die ist dein Smartphone")
 
+@when("smartphone benutzen")
+@when("benutzen smartphone")
+@when("benutze smartphone")
+def benutze_smartphone():
+    if (room_number == 1):
+        print("Du hast keinen Empfang, aber die Kamera funktioniert noch")
+    else:
+        print("Du versuchst deine bessere Hälfte anzurufen, um sie vor der Kernschmelze zu warnen. Aber du erreichst nur die Mailbox")
+
+@when("smartphone öffnen")
+@when("öffnen smartphone")
+@when("öffne smartphone")
+def oeffne_smartphone():
+    if (sicherheitstuer_offen == False):
+        print("Du kannst das Smartphone nicht öffnen")
+
+@when("nimm smartphone")
+@when("nehmen smartphone")
+@when("smartphone nehmen")
+def nehme_smartphone():
+    print("Das Smartphone befindet sich schon in deinem Inventar")
+
+@when("smartphone verwenden")
+@when("verwenden smartphone")
+@when("verwende smartphone")
+def verwende_smartphone():
+    print("Womit möchtest du das Smartphone verwenden")
+    print("zB verwende Smartphone mit dir")
+
+@when("selfie")
+@when("verwende mit smartphone dir")
+@when("verwende smartphone mit dir")
+def verwende_smartphone_mit_dir():
+    print("Du machst ein Selfie von dir")
+
+
+
+
+### Räume ###
+#aktuell von mir unbenutzt, kann das weg???
 room1 = Room("""Beschreibung des Kontrollraums""")
 room2 = Room("""Beschreibung des Maschinenraums""")
 room3 = Room("""Beschreibung des Flurs""")
@@ -131,7 +227,6 @@ input("...")
 # RAUM 1: KONTROLLRAUM #
 ########################
 # Einleitung Raum 1:
-# Befehle: umschauen anschauen nehmen benutzen öffnen 'verwende mit' Inventar help quit
 say(
     """----------------------------------------------------------------------------------"""
 )
@@ -145,15 +240,11 @@ Du kannst dein aktuelles [Inventar] anschauen\n
 Du kannst dir [help] suchen, wenn du nicht weiterkommst, aber Vorsicht, dies ist eine sehr große Hilfe!\n
 Du kannst mit [quit] das AKW verlassen (Spiel beenden)\n"""
 )
-# Startumgebung festlegen
-inventory.add(smartphone)
-set_context("room1")
-room_number = 1
 
 # Look Around #
 @when("umschauen", context="room1")
-#@when("schaue um", context="room1")
-#@when("schau dich um", context="room1")
+@when("schaue um", context="room1")
+@when("schau dich um", context="room1")
 def look_around_room1():
     # umschauen in Raum 1
     if sicherheitstuer_offen == False:
@@ -173,6 +264,117 @@ def look_around_room1():
         Des Weiteren erblickst du [Ministerin Schrader], die den ohnmächtigen [Herr Solar] in die stabile Seitenlage gebracht hat, und in einer weiteren Ecke das [Fernsehteam].""")
         
 
+### Gegenstände Raum 1 ###
+
+#Poster anschauen nehmen benutzen öffnen, kein verwende mit
+
+@when("anschauen poster", context="room1")
+@when("poster", context="room1")
+@when("schaue poster an", context="room1")
+def zeige_poster():
+    say("""Das Poster trägt die Aufschrift „How much is the fish“. Es scheint mit Klebestreifen befestigt worden zu sein. Von der Rückseite schimmert Schrift durch das dünne Papier.""")
+
+@when("poster nehmen", context="room1")
+@when("nimm poster", context="room1")
+@when("nehme poster", context="room1")
+@when("nehmen poster", context="room1")
+def nehme_poster():
+    say("""Es ist mit Klebestreifen an der Wand befestigt. Die oberen Klebenstreifen haften zu stark und du möchstest es nicht zerstören. Du kannst aber einen Blick auf die Rückseite erhaschen. Du findest eine Widmung an Herrn Solar mit der Unterschrift von HP Baxxter und einer kurzen Biografie des Künstlers unter anderem mit seinem Geburtsdatum:""")
+    say("""16. März 1964""")
+    #zur erhöhung des Schwierigkeitsgrades, noch weitere Daten einbauen: siehe Confluence
+
+@when("poster öffnen", context="room1")
+@when("öffne poster", context="room1")
+def oeffne_poster():
+    print("Du kannst das Poster nicht öffnen!")
+
+@when("poster benutzen", context="room1")
+@when("benutze poster", context="room1")
+def benutze_poster():
+    print("Du kannst das Poster nicht benutzen!")
+
+#Sicherheitsausrüstung anschauen nehmen benutzen öffnen, kein verwende mit
+
+@when("schaue sicherheitsausrüstung an", context="room1")
+@when("sicherheitsausrüstung", context="room1")
+@when("anschauen sicherheitsausrüstung", context="room1")
+def zeige_sicherheitsausruestung():
+    global check_sicherheitsausruestung
+    check_sicherheitsausruestung = True
+    if inventory.find("brecheisen") is None:
+        say("""Du durchsuchst die Sicherheitsausrüstung am Ende des Raumes. Dabei findest du ein Brecheisen.""")
+    else:
+        say("""Du durchsuchst die Sicherheitsausrüstung am Ende des Raumes. Dabei findest nichts brauchbares.""")
+
+@when("sicherheitsausrüstung nehmen", context="room1")
+@when("nimm sicherheitsausrüstung", context="room1")
+@when("nehme sicherheitsausrüstung", context="room1")
+@when("nehmen sicherheitsausrüstung", context="room1")
+def nehme_sicherheitsausruestung():
+    global check_sicherheitsausruestung
+    if (check_sicherheitsausruestung == True and inventory.find("brecheisen") is None):
+        nehme_brecheisen()
+    else:
+        print("Du kannst die Sicherheitsausrüstung nicht nehmen")
+
+@when("sicherheitsausrüstung benutzen", context="room1")
+@when("benutze sicherheitsausrüstung", context="room1")
+def benutze_sicherheitsausruestung():
+    print("Du kannst die Sicherheitsausrüstung nicht benutzen!")
+
+@when("sicherheitsausrüstung öffnen", context="room1")
+@when("öffne sicherheitsausrüstung", context="room1")
+def oeffne_sicherheitsausruestung():
+    print("Du kannst die Sicherheitsausrüstung nicht öffnen!")
+
+#Brecheisen Inventar anschauen nehmen benutzen öffnen, noch kein verwende mit (kommt später)
+
+@when("schaue brecheisen an")
+@when("brecheisen")
+@when("anschauen brecheisen")
+def zeige_brecheisen():
+    global check_sicherheitsausruestung
+    if ((check_sicherheitsausruestung == True and room_number == 1) or inventory.find("brecheisen") is not None):
+        print("Dies ist ein Brecheisen")
+    else:
+        print("Welches Brecheisen?")
+
+@when("brecheisen nehmen", context="room1")
+@when("nimm brecheisen", context="room1")
+@when("nehme brecheisen", context="room1")
+@when("nehmen brecheisen", context="room1")
+def nehme_brecheisen():
+    if (inventory.find("brecheisen") is not None):
+        print("Das Brecheisen befindet sich schon in deinem Inventar")
+    elif (check_sicherheitsausruestung == True):
+        inventory.add(brecheisen)
+        say("""„Das könnte eventuell noch nützlich sein“, sagst du und packst das Brecheisen direkt ein.""")
+        say("""Jetzt fühlst du dich wie Gordon Freeman""")
+    else:
+        print("Welches Brecheisen?")
+
+@when("brecheisen benutzen")
+@when("benutze brecheisen")
+def benutze_brecheisen():
+    global brecheisen_schienbein
+    if (inventory.find("brecheisen") is not None):
+        if (brecheisen_schienbein == False):
+            say("""Aus Wut möchtest du etwas kaputt machen. Du suchst nach dem nächst besten Gegenstand und holst mit dem Brecheisen aus. Dabei verlierst du das Gleichgewicht und haust dir das Brecheisen gegen das Schienbein.""")
+            say("""AUA""")
+            say("""Du bist wohl doch kein Gordon Freeman ;-)""")
+            brecheisen_schienbein = True
+        else:
+            print("Dein Schienbein schmerzt immer noch")
+    else:
+        print("Du hast kein Brecheisen in deinem Inventar")
+
+@when("brecheisen öffnen")
+@when("öffne brecheisen")
+def oeffne_sicherheitsausruestung():
+    print("Du kannst das Brecheisen nicht öffnen!")
+
+
+### Ende der Raume ### nun nur noch Debug und der Start-Aufruf
 
 # Debug #
 
@@ -184,15 +386,21 @@ def debug():
     debug_input = input("In welchen Raum springen? ")
     if debug_input == "1":
         set_context("room1")
+        room_number = 1
     elif debug_input == "2":
+        room_number = 2
         set_context("room2")
     elif debug_input == "3":
+        room_number = 3
         set_context("room3")
     elif debug_input == "4":
+        room_number = 5
         set_context("room4")
     elif debug_input == "5":
+        room_number = 5
         set_context("room5")
     elif debug_input == "6":
+        room_number = 6
         set_context("room6")
 
 
@@ -212,8 +420,15 @@ def debug2():
 
 
 ## start ###
+
+### Startumgebung festlegen ###
+inventory.add(smartphone)
+set_context("room1")
+room_number = 1
+
 start()
+# start(help = False) um den Help Befehl auszuschalten
 
 
-def no_command_matches(command):
-    print("Das habe ich nicht verstanden")
+
+
