3510cff52e6a375735f0142ca1fe436783139119
Lasslos05 <laslo.hauschild@web.de>
2022-03-17 18:01:42 -0600

    Bug fixes

--- room_6.py
+++ room_6.py
@@ -275,7 +275,7 @@ def firewall_schliessen():
                 print_loesung_firewall()
                 time.sleep(4.0)
                 abspann()
-            elif input_loesung == "exit":
+            elif input_loesung == "zurück" or input_loesung == "exit":
                 return
             else:
                 say(
